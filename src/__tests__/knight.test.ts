bbc96eb77d12525e38d408ff686953ceb24a65a0
Caaina Nascimento <caaina@lsd.ufcg.edu.br>
2020-03-06 15:33:28 -0300

    More knight test cases

--- src/__tests__/knight.test.ts
+++ src/__tests__/knight.test.ts
@@ -2,28 +2,41 @@ import Knight from '../services/knight'
 import ValidMovesRequest from '../common/ValidMovesRequest'
 
 describe('Knight Service', () => {
+  const defaultRequest: ValidMovesRequest = { turns: 2, position: 'A5' }
+  const defaultKnight: Knight = new Knight(defaultRequest)
   it('has ValidMoves function', () => {
-    expect(Knight.ValidMoves).toBeDefined()
+    expect(defaultKnight.ValidMoves).toBeDefined()
   })
   it('must return a ValidMovesRequestObject', () => {
-    const request: ValidMovesRequest = { position: 'A5' }
-    const response = Knight.ValidMoves(request)
+    const response = defaultKnight.ValidMoves()
     expect(response).toHaveProperty('positions')
     expect(Array.isArray(response.positions)).toBe(true)
   })
   it('must throw a error for non valid algebraic position', () => {
-    const request: ValidMovesRequest = { position: 'M99' }
+    const request: ValidMovesRequest = { turns: 2, position: 'M99' }
     try {
-      Knight.ValidMoves(request)
+      const knight: Knight = new Knight(request)
+      knight.ValidMoves()
     } catch (e) {
       expect(e.message).toBe('INVALID_POSITION')
     }
   })
   it('must throw a error for request without position', () => {
+    const request: ValidMovesRequest = { turns: 2, position: '' }
     try {
-      Knight.ValidMoves({ position: '' })
+      const knight: Knight = new Knight(request)
+      knight.ValidMoves()
     } catch (e) {
       expect(e.message).toBe('POSITION_NOT_FOUND')
     }
+  })
+  it('must return the correct possible positions', () => {
+    const request: ValidMovesRequest = { turns: 2, position: 'A1' }
+    const possibleMoves = ['A1', 'C1', 'D2', 'A5', 'C5', 'D4', 'E1', 'A3', 'B4', 'E3']
+    const knight: Knight = new Knight(request)
+    const response = knight.ValidMoves()
+    expect(response).toHaveProperty('positions')
+    expect(response.positions.length).toBe(10)
+    possibleMoves.forEach(possibleMove => expect(response.positions.indexOf(possibleMove)).toBeGreaterThanOrEqual(0))
   })
 })
