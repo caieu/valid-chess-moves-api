49d5aff3b5306224c914931603bc6962be2f62c2
Caaina Jeronimo <caainaje@gmail.com>
2020-03-08 23:25:35 -0300

    fix prod config, no watch

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.config.ts')
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  watch: false
 })
